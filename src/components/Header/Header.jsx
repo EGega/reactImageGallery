856baecc095f84dd2b3b3ce7c24092a5df09ac5f
gegaenea@yahoo.com <gegaenea@yahoo.com>
2022-10-09 22:16:54 +0300

    React Image Gallery

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.scss'
+
+const 
+Popular = () => {
+  return (
+    <div className='header'>
+      Popular Tour Places
+    </div>
+  )
+}
+
+export default Popular
